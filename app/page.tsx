e824a006bcae5a153f76dc50e6ba5aa558d71df8
“fedulovs” <heaven45@mail.ru>
2024-03-03 14:22:28 +0200

    change H1

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export const Home = () => {
-    return <h1>Привет!</h1>;
+    return <h1>Main Page</h1>;
 };
 
 export default Home;
