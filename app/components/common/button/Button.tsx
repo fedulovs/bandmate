6622a4beb185f5247845f6a083eef1ed36577d38
fedulovs <heaven45@mail.ru>
2024-05-11 14:36:34 +0300

    make button accept additional classnames

--- app/components/common/button/Button.tsx
+++ app/components/common/button/Button.tsx
@@ -3,22 +3,24 @@ import './button.css';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     buttonType: 'regular' | 'login';
+    classNames?: string;
 }
 
 export const Button = (props: Props) => {
-    switch (props.buttonType) {
-        case 'regular': {
-            return (
-                <button className='button-normal' {...props}>
-                    {props.children}
-                </button>
-            );
-        }
-        case 'login': {
-            return <button className='button-login'>{props.children}</button>;
-        }
-        default: {
-            return <button className='button-normal'>{props.children}</button>;
-        }
+    const { buttonType, classNames, children, ...rest } = props;
+
+    let className = '';
+    if (props.buttonType === 'regular') {
+        className = 'button-normal';
+    } else if (props.buttonType === 'login') {
+        className = 'button-login';
     }
+
+    className = `${className} ${classNames || ''}`;
+
+    return (
+        <button className={className.trim()} {...rest}>
+            {children}
+        </button>
+    );
 };
