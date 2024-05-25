f9ab6aa32a052adaef1f9fd65d19fe510a565b54
fedulovs <heaven45@mail.ru>
2024-05-25 15:33:35 +0300

    display all user bands in dropdown

--- app/components/common/drpopdown/Dropdown.tsx
+++ app/components/common/drpopdown/Dropdown.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './dropdown.css';
 import Link from 'next/link';
+import { useAppSelector } from '@/app/store/store';
 
 const Dropdown = () => {
+    const user = useAppSelector((state: any) => state.user);
+
     return (
         <div className='dropdown-container'>
             <Link href='/profile'>
                 <ul className='dropdown-item'>Profile</ul>
             </Link>
+            <p className='bands-heading'>Bands</p>
             <Link href='/band'>
-                <ul className='dropdown-item'>Band</ul>
+                {user.bands.map((band: string) => (
+                    <ul className='dropdown-item dropdown-band'>{band}</ul>
+                ))}
             </Link>
             <Link href='/auth/login'>
                 <ul className='dropdown-item'>Exit</ul>
